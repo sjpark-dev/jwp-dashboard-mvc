9b5ce2712b7f8e66fc766f82c7d93798c9c88833
sjpark-dev <66653739+sjpark-dev@users.noreply.github.com>
2021-09-16 13:42:18 +0900

    [MVC 프레임워크 구현하기 - 2,3단계] 에드(박성진) 미션 제출합니다. (#64)
    * feat: Reflection 학습 테스트 작성
    
    * feat: JUnit3 학습 테스트 작성
    
    * feat: JUnit4 학습 테스트 작성
    
    * feat: Reflections 학습 테스트 작성
    
    * feat: AnnotationHandlerMapping 구현
    
    * feat: JspView 구현
    
    * feat: Legacy MVC와 Annotation Based MVC 통합
    
    * feat: 로거 상수명 변경
    
    * feat: HandlerAdapter 구현
    
    * refactor: HandlerAdapterRegister 적용
    
    * refactor: HandlerMappingRegister 적용
    
    * refactor: ControllerScanner 적용
    
    * refactor: DispatcherServlet render 로직 분리
    
    * feat: ControllerScanner 테스트 코드 작성
    
    * feat: HandlerMappingRegister 테스트 코드 작성
    
    * feat: HandlerAdapterRegister 테스트 코드 작성
    
    * feat: JsonView 구현
    
    * feat: RedirectView 구현
    
    * refactor: JspView 중복 변수 제거
    
    * refactor: JsonView 예외 핸들링 & ObjectMapper 상수화
    
    * refactor: legacy controller -> Annotation controller 전환

--- mvc/src/main/java/nextstep/mvc/view/JsonView.java
+++ mvc/src/main/java/nextstep/mvc/view/JsonView.java
@@ -1,13 +1,42 @@
 package nextstep.mvc.view;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.PrintWriter;
 import java.util.Map;
+import nextstep.web.support.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JsonView implements View {
 
+    private static final Logger LOG = LoggerFactory.getLogger(JsonView.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
     @Override
-    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) {
+        if (model.size() <= 1) {
+            model.keySet().forEach(key -> {
+                LOG.debug("attribute name : {}, value : {}", key, model.get(key));
+                write(model.get(key), response);
+            });
+            return;
+        }
+
+        write(model, response);
+    }
+
+    private void write(Object object, HttpServletResponse response) {
+        try {
+            PrintWriter writer = response.getWriter();
+            response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            String jsonString = OBJECT_MAPPER.writeValueAsString(object);
+            writer.print(jsonString);
+            writer.flush();
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
     }
 }
--- mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
+++ mvc/src/test/java/nextstep/mvc/controller/tobe/ControllerScannerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import samples.TestController;
 import samples.TestController2;
 import samples.TestController3;
+import samples.TestController4;
+import samples.TestController5;
 
 class ControllerScannerTest {
 
@@ -16,7 +18,8 @@ class ControllerScannerTest {
         final ControllerScanner controllerScanner = new ControllerScanner(basePackage);
         final Map<Class<?>, Object> controllers = controllerScanner.getControllers();
 
-        Class<?>[] expected = {TestController.class, TestController2.class, TestController3.class};
+        Class<?>[] expected = {TestController.class, TestController2.class, TestController3.class,
+                TestController4.class, TestController5.class};
         assertThat(controllers.keySet()).containsExactlyInAnyOrder(expected);
     }
 }
